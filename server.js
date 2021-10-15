3294879f9087d95e31b71e7b26c3c344de62be15
Ursula Wilborn <wilbornu@yahoo.com>
2021-10-14 23:13:10 -0400

    update

--- server.js
+++ server.js
@@ -25,4 +25,5 @@ app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
+  console.log("MongoDB database connection established successfully");
 });
